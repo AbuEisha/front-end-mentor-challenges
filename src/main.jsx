222d07a7f1a4035c4c91e894f9e96a9bb1876b33
Mustafa Abu Eisha <mostafaabuasha321@gmail.com>
2025-12-02 10:51:46 +0200

    Fix merge conflict in main.jsx
    Removed merge conflict markers and cleaned up imports.

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,6 @@
-<<<<<<< HEAD
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
-=======
-import { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
-import App from "./App.jsx";
->>>>>>> af456f4 (Update Some Challenge Files)
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
